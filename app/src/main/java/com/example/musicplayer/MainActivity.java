84ad3e5a6b39cc751070328f1863673b5d73b845
sk-zayed <sk.zayedd@gmail.com>
2022-06-23 13:12:05 +0530

    MusicPlayer

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        ListView listView;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.song_list);
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        ArrayList<File> mySongs = fetchsongs(Environment.getExternalStorageDirectory());
+                        String [] items = new String[mySongs.size()];
+                        for(int i=0;i<mySongs.size();i++){
+                            items[i] = mySongs.get(i).getName().replace(".mp3", "");
+                        }
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.text_clr, items);
+                        listView.setAdapter(adapter);
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                Intent intent = new Intent(MainActivity.this, PlaySong.class);
+                                String currentSong = listView.getItemAtPosition(position).toString();
+                                intent.putExtra("songList", mySongs);
+                                intent.putExtra("currentSong", currentSong);
+                                intent.putExtra("position", position);
+                                startActivity(intent);
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check();
+
+    }
+
+    public ArrayList<File> fetchsongs(File file){
+        ArrayList arrayList = new ArrayList();
+        File [] songs = file.listFiles();
+        if(songs != null){
+            for(File myFile: songs){
+                if(!myFile.isHidden() && myFile.isDirectory()){
+                    arrayList.addAll(fetchsongs(myFile));
+                }
+                else{
+                    if(myFile.getName().endsWith(".mp3") && !myFile.getName().startsWith(".")){
+                        arrayList.add(myFile);
+                    }
+                }
+            }
+        }
+        Arrays.sort(new ArrayList[]{arrayList});
+        return arrayList;
+    }
+}
